2449e43d64c5c32bb9a26bdf93f06df843b559d8
Adrian <kemski12321@gmail.com>
2023-09-25 22:17:35 +0100

    zadanie w polowie zakonczone

---
+++ database/migrations/2023_09_25_193450_create_employee_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained(
+                table: 'companies', indexName: 'id'
+            )->onDelete('cascade');
+            $table->string('imie');
+            $table->string('nazwisko');
+            $table->string('email');
+            $table->string('numer_telefonu')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
